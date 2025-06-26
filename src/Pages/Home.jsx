2e6d6d1e06246ee352d8dec7195d4d08f36854c3
Marquan Bowman <55892092+MarquanBow@users.noreply.github.com>
2025-06-26 18:05:37 -0400

    fix image

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
       >
       
 <motion.img
-  src="../public/Profile.jpg"
+  src="/Profile.jpg"
   alt="Marquan Bowman"
   className={styles.profile}
   initial={{ opacity: 0, scale: 0.9 }}
